60a870356ee290a422496bc1353611696cb5199c
Zac Cooper <zacooper5g@outlook.com>
2022-10-05 15:31:50 +1100

    added player and enemy, gravity effects added

--- public/index.js
+++ public/index.js
@@ -8,23 +8,32 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2;
+
 // creating sprite objects
 
 class Sprite {
     constructor({ position, velocity }) {
         this.position = position;
         this.velocity = velocity;
-    }
+        this.height = 150;
+    };
 
     draw() {
         c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 50, 150);
-    }
+        c.fillRect(this.position.x, this.position.y, 50, this.height);
+    };
 
     update() {
         this.draw();
-        this.position.y += 10;
-    }
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0;
+        } else {
+            this.velocity.y += gravity;
+        }
+    };
 };
 
 const player = new Sprite({
@@ -34,8 +43,9 @@ const player = new Sprite({
     },
     velocity: {
         x: 0,
-        y: 0,
-    }
+        y: 10,
+    },
+
 });
 
 const enemy = new Sprite({
@@ -59,4 +69,4 @@ const animate = () => {
     enemy.update();
 };
 
-animate()
+animate();
