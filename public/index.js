4ade6537d257661e9d779155cbdd1fc593d305df
Zac Cooper <zacooper5g@outlook.com>
2022-10-06 16:17:02 +1100

    added timer and gameover mechanics

--- public/index.js
+++ public/index.js
@@ -124,6 +124,8 @@ const keys = {
     }
 };
 
+// collision detector function
+
 const rectangularCollision = ({
     rectangle1,
     rectangle2,
@@ -134,7 +136,33 @@ const rectangularCollision = ({
         rectangle1.hitBox.position.y + rectangle1.hitBox.height >= rectangle2.position.y &&
         rectangle1.hitBox.position.y <= rectangle2.position.y + rectangle2.height
     )
-}
+};
+
+// timer funtion and end game function
+
+let timer = 45;
+const decreaseTimer = () => {
+    setTimeout(decreaseTimer, 1000)
+    if (timer > 0) {
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    } else timer = 0;
+
+    if (player.health === enemy.health && timer === 0) {
+        document.querySelector('#result').style.display = 'flex'
+        document.querySelector('#result').innerHTML = 'Draw'
+    } else if (player.health > enemy.health && timer === 0 || enemy.health <= 0) {
+        document.querySelector('#result').style.display = 'flex'
+        document.querySelector('#result').innerHTML = 'Player 1 Wins'
+        timer = 0;
+    } else if (player.health < enemy.health && timer === 0 || player.health <= 0) {
+        document.querySelector('#result').style.display = 'flex'
+        document.querySelector('#result').innerHTML = 'Player 2 Wins'
+        timer = 0;
+    };
+};
+
+decreaseTimer();
 
 const animate = () => {
     window.requestAnimationFrame(animate);
@@ -178,7 +206,7 @@ const animate = () => {
 
     ) {
         player.isAttacking = false;
-        enemy.health -= 15;
+        enemy.health -= 20;
         document.querySelector("#enemyHealth").style.width = enemy.health + '%'
     };
 
@@ -191,7 +219,7 @@ const animate = () => {
 
     ) {
         enemy.isAttacking = false;
-        player.health -= 15;
+        player.health -= 20;
         document.querySelector("#playerHealth").style.width = player.health + '%'
     }
 };
